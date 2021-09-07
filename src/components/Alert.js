22c3013da5b017cef80b90ded3efeda7291e4231
Bhavya Khandelwal <84895510+Bhavya-Khandelwal@users.noreply.github.com>
2021-09-07 16:21:37 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,13 +9,17 @@ import React from "react";
   return (
     
       
+      <div style={{height:'50px'}}>
       props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
       
       
       <strong>{Capitalize(props.alert.types)}</strong>:{props.alert.msg}
+      
 
      
     </div>
+    </div>
+   
    
   );
 }
